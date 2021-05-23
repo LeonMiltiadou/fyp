044f50629c6dcdec96247ad100852a0900a0a09c
LeonMiltiadou <leon.miltiadou@hotmail.co.uk>
2021-05-23 18:44:52 +0100

    force build

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import Head from 'next/head';
 import { AppWrapper } from '../context/state.js';
 import CookieConsent from "react-cookie-consent";
 
-
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css';
